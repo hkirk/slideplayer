b5be1450ee8ac4225ca46ac8c1fe4ad67f1b4908
Henrik Kirk <henrik@busywait.org>
2020-03-03 06:50:28 +0100

    Added linux specific cc files

---
+++ flutter_app/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,15 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <file_chooser_plugin.h>
+#include <menubar_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FileChooserPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileChooserPlugin"));
+  MenubarPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MenubarPlugin"));
+}
